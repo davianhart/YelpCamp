bf6a9dbcffbed9eae64be8c7079ba5ac9adb8fb0
davianhart <55667626+davianhart@users.noreply.github.com>
2020-08-10 12:05:28 -0400

    update port listening

--- app.js
+++ app.js
@@ -55,8 +55,9 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("YelpCamp server has started.");
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+ console.log("Server Has Started!");
 });
 
 // seedDB(); // SEED THE DATABASE
